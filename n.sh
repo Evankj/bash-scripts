4e99d450991e572f28cbfccdb8bac28f8b039c73
Evankj <evankellyjohnston@gmail.com>
2022-11-14 11:34:36 +1100

    Add new note script

---
+++ n.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+trim() {
+    local var="$*"
+    # remove leading whitespace characters
+    var="${var#"${var%%[![:space:]]*}"}"
+    # remove trailing whitespace characters
+    var="${var%"${var##*[![:space:]]}"}"
+    echo "$var"
+}
+
+title=$(trim $1)
+if [ ! -z "$title" ]
+then
+  title=$(echo "_$title")
+fi
+
+d=$(date +"%Y%m%d")
+
+nvim ~/vimwiki/random/"$d$title".md
