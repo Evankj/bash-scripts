c742d0ae9110ad4d8c5845610e0e3a4c0a1bd292
Evankj <evankellyjohnston@gmail.com>
2022-11-14 12:41:02 +1100

    Fix vw live grep.

--- vw.sh
+++ vw.sh
@@ -2,7 +2,7 @@
 str="$*"
 if [ $# -eq 0 ]
 then
-  lvim ~/vimwiki +"Telescope find_files cwd=~/vimwiki/"
+  lvim ~/vimwiki +"lua require('telescope.builtin').find_files({ cwd = '~/vimwiki/', prompt_title = 'Search VimWiki...'})"
 else
-  lvim ~/vimwiki +"Telescope live_grep cwd=~/vimwiki/ default_text=$str"
+  lvim ~/vimwiki +"lua require('telescope.builtin').live_grep({ cwd = '~/vimwiki/', default_text = '$str'})"
 fi
